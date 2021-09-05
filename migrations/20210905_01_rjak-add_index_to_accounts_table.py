caa331222b911eb0f05cd6c2d6abda1926020fe5
Quist0n <37319086+Quist0n@users.noreply.github.com>
2021-09-05 14:36:17 -0500

    optimization(db): Added indexes for account table on columns 'username', 'created_at' and 'role'

---
+++ migrations/20210905_01_rjak-add_index_to_accounts_table.py
@@ -0,0 +1,14 @@
+
+"""
+Add index to account table
+"""
+from yoyo import step
+__depends__ = {"20210814_01_xwCee_add_role_column_to_accounts_table"}
+
+steps = [
+    step(
+    "CREATE INDEX IF NOT EXISTS account_idx USING BTREE ON account( username, created_at, role);",
+
+    "DROP INDEX IF EXISTS account_info_idx;"
+    )
+    ]
